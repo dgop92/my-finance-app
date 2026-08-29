03d9b77224d77c66d1fd3ddc6aa7625ea7580c95
dgop92 <inevaup@gmail.com>
2026-08-29 18:28:48 -0500

    feat: remove old code

--- src/features/core/index.ts
+++
@@ -1,9 +0,0 @@
-// Entity exports
-export * from "./entities/saving-source";
-export * from "./entities/savings-source-value";
-export * from "./entities/financial-record";
-export * from "./entities/expense";
-
-// Repository exports
-export * from "./repositories/savings-source.repository";
-export * from "./repositories/financial-record.repository";
